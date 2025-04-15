912ea403f087cc0598f3b8bea864aae8d2c6f9ec
my name <tainv1101@gmail.com>
2025-04-15 16:16:23 +0700

    fix bug loading react query and update text

--- features/practice/layout/DemoHeader.tsx
+++ features/practice/layout/DemoHeader.tsx
@@ -1,14 +1,14 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Link } from "@/i18n/routing";
 
 function DemoHeader() {
   return (
-    // <div className="relative inset-x-0 top-0 h-20">
     <div className="sticky z-50 inset-0 flex items-center justify-start gap-2 px-8 py-3 bg-white border-b-2 border-accent text-primary">
       <SidebarTrigger className="hover:bg-primary hover:text-white" />
-      <h1 className="text-2xl font-bold hover:text-accent">Nguyễn Văn Tài</h1>
+      <Link href="/">
+        <h1 className="text-2xl font-bold hover:text-accent">Nguyen Van Tai</h1>
+      </Link>
     </div>
-    // </div>
-
   );
 }
 
